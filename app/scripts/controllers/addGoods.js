c690901fc9c47ee3152f86b459b9874d833b2efa
sigh <wangximingde@163.com>
2014-09-04 15:58:43 +0800

    fix bug

--- app/scripts/controllers/addGoods.js
+++ app/scripts/controllers/addGoods.js
@@ -2,7 +2,6 @@
 angular.module('yeomanLetusgoApp')
   .controller('AddGoodsCtrl', function ($scope, goodsListService, catagaryService) {
     $scope.add = function (goods) {
-      console.log(goods);
       goodsListService.addGoods(goods);
       $scope.$emit('refreshGoodsList');
       $scope.$broadcast('refreshGoodsList');

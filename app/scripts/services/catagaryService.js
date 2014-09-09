a89db083e05ef6fcc507820c95170bdaf0a2f3e3
sigh <wangximingde@163.com>
2014-09-09 10:04:06 +0800

    format code

--- app/scripts/services/catagaryService.js
+++ app/scripts/services/catagaryService.js
@@ -1,16 +1,28 @@
-'use strict'
+'use strict';
+(function (_) {
+  angular.module('yeomanLetusgoApp')
+    .service('catagaryService', function (localStorageService) {
+      this.catagarys = localStorageService.get('catagarys');
+      this.getCatagarys = function () {
+        return localStorageService.get('catagarys');
+      };
 
-angular.module('yeomanLetusgoApp')
-  .service('catagaryService', function (localStorageService) {
-    this.getCatagarys = function () {
-      return localStorageService.get('catagarys');
-    };
+      this.storeCatagary = function (catagary) {
+        this.temporaryCatagary = catagary;
+      };
 
-    this.storeCatagary = function (catagary) {
-      this.temporaryCatagary = catagary;
-    };
+      this.getStoreCatagary = function () {
+        return this.temporaryCatagary;
+      };
 
-    this.getStoreCatagary = function () {
-      return this.temporaryCatagary;
-    };
-  });
+      this.editCatagary = function (catagary) {
+        var index = _.findIndex(catagarys, {'id': catagary.id});
+        this.catagarys[index] = catagary;
+        this.editGoodsList(catagarys);
+      };
+
+      this.editCatagarys = function (catagarys) {
+        localStorageService.set('catagarys', catagarys);
+      }
+    });
+})(window._);

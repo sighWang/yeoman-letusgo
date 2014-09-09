84ea9a77c147695ea4a331f5fc142711bca839b0
sigh <wangximingde@163.com>
2014-09-09 11:11:58 +0800

    format code

--- app/scripts/services/goodsListService.js
+++ app/scripts/services/goodsListService.js
@@ -1,41 +1,41 @@
 'use strict';
 (function (_) {
-angular.module('yeomanLetusgoApp')
-  .service('goodsListService', function GoodsListService(localStorageService) {
-    this.goodsList = localStorageService.get('goodsList');
-    this.getGoodslist = function () {
-      return localStorageService.get('goodsList');
-    };
+  angular.module('yeomanLetusgoApp')
+    .service('goodsListService', function GoodsListService(localStorageService) {
+      this.goodsList = localStorageService.get('goodsList');
+      this.getGoodslist = function () {
+        return localStorageService.get('goodsList');
+      };
 
-    this.storeGoods = function (goods) {
-      this.temporaryGoods = goods;
-    };
+      this.storeGoods = function (goods) {
+        this.temporaryGoods = goods;
+      };
 
-    this.getStoreGoods = function () {
-      return this.temporaryGoods;
-    };
+      this.getStoreGoods = function () {
+        return this.temporaryGoods;
+      };
 
-    this.editGoodsList = function (goodsList) {
-      localStorageService.set('goodsList', goodsList);
-    };
+      this.editGoodsList = function (goodsList) {
+        localStorageService.set('goodsList', goodsList);
+      };
 
-    this.editGoods = function (goods) {
-      var goodsList = this.goodsList;
-      var index = _.findIndex(goodsList, {'id': goods.id});
-      this.goodsList[index] = goods;
-      this.editGoodsList(goodsList);
-    };
+      this.editGoods = function (goods) {
+        var goodsList = this.goodsList;
+        var index = _.findIndex(goodsList, {'id': goods.id});
+        this.goodsList[index] = goods;
+        this.editGoodsList(goodsList);
+      };
 
-    this.addGoods = function (goods) {
-      var goodsList = this.goodsList;
-      goodsList.push(goods);
-      this.editGoodsList(goodsList);
-    };
-    this.removeGoods = function (goods) {
-      var goodsList = this.goodsList;
-      var index = _.findIndex(goodsList, {'id': goods.id});
-      goodsList.splice(index, 1);
-      this.editGoodsList(goodsList);
-    };
-  });
+      this.addGoods = function (goods) {
+        var goodsList = this.goodsList;
+        goodsList.push(goods);
+        this.editGoodsList(goodsList);
+      };
+      this.removeGoods = function (goods) {
+        var goodsList = this.goodsList;
+        var index = _.findIndex(goodsList, {'id': goods.id});
+        goodsList.splice(index, 1);
+        this.editGoodsList(goodsList);
+      };
+    });
 })(window._);

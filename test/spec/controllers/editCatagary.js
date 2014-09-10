08010bd32a286e110dc32514d63564b7fd0ccdab
sigh <wangximingde@163.com>
2014-09-10 17:26:51 +0800

    complete editCatagary test

---
+++ test/spec/controllers/editCatagary.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Controller: EditCatagaryCtrl', function () {
+  var $scope, createController, catagaryService,  $controller;
+
+  beforeEach(function () {
+    module('yeomanLetusgoApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      $controller = $injector.get('$controller');
+      catagaryService = $injector.get('catagaryService');
+
+      createController = function () {
+        return $controller('EditCatagaryCtrl', {
+          $scope: $scope
+        });
+      };
+    });
+  });
+
+  describe('editCatagary.js', function () {
+    it('catagary should be get', function () {
+      spyOn(catagaryService, 'getStoreCatagary').andReturn({id:'4', name:'5'});
+      createController();
+      expect($scope.catagary).toEqual(jasmine.any(Object));
+    });
+    it('catagaryService.editCatagary should be called', function () {
+      spyOn(catagaryService, 'editCatagary');
+      createController();
+      $scope.edit({id:'4', name:'5'});
+      expect(catagaryService.editCatagary).toHaveBeenCalledWith({id:'4', name:'5'});
+    })
+  });
+});
